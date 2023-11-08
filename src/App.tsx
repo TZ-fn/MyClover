b2a926b69e17c550c33e515676505b32f477458d
Tomek <tomekz@vp.pl>
2023-11-08 15:21:14 +0100

    chore: initial commit, remove the boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import mainLogo from "./assets/mainLogo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Clover Family Cover</h1>
+      <img src={mainLogo} className="logo" />
+    </>
+  );
+}
+
+export default App;
