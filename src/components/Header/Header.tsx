fb419a966279b6ead64f4dd860a3bb03a530778d
Tomek <tomekz@vp.pl>
2023-11-23 11:48:39 +0100

    fix: add missing link description and fix the contrast ratio in menu text to improve the accessibility

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ function Header() {
   return (
     <header className={styles.header}>
       <a href="/">
+        <span className="visually-hidden">Przejdź do strony głównej.</span>
         <img src={mainLogo} className={styles.mainLogo} alt="My Clover Logo" />
       </a>
       <NavigationList />
