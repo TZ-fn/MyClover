2182f0c2b4f6217a67c081ff0822dd408b79192d
Tomek <tomekz@vp.pl>
2023-11-28 16:17:58 +0100

    feat: add translations data

---
+++ src/assets/translations/englishTranslation.ts
@@ -0,0 +1,3 @@
+const englishTranslation = {};
+
+export default englishTranslation;
---
+++ src/assets/translations/polishTranslation.ts
@@ -0,0 +1,12 @@
+const polishTranslation = {
+  pages: {
+    home: {
+      header: {
+        mainLogoLink: "Przejdź do strony głównej.",
+        mainLogoAlt: "Logo My Clover",
+      },
+    },
+  },
+};
+
+export default polishTranslation;
