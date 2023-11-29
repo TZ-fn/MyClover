d319d06f2c99e72320b7fc0a3a4a97eb447434c1
Tomek <tomekz@vp.pl>
2023-11-29 14:39:09 +0100

    feat: switch text data to use translations

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
 import mainLogo from "assets/mainLogo.png";
 import NavigationList from "./NavigationList/NavigationList";
 import styles from "./Header.module.scss";
+import useTranslation from "~/hooks/useTranslation";
 
 function Header() {
+  const [translation] = useTranslation();
+
   return (
     <header className={styles.header}>
       <a href="/">
-        <span className="visually-hidden">Przejdź do strony głównej.</span>
-        <img src={mainLogo} className={styles.mainLogo} alt="My Clover Logo" />
+        <span className="visually-hidden">{translation.layout.header.mainLogoLink}</span>
+        <img src={mainLogo} className={styles.mainLogo} alt={translation.layout.header.mainLogoAlt} />
       </a>
       <NavigationList />
     </header>
--- src/components/Header/NavigationList/NavigationList.tsx
+++ src/components/Header/NavigationList/NavigationList.tsx
@@ -3,15 +3,23 @@ import { Transition } from "react-transition-group";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import styles from "./NavigationList.module.scss";
 import useWindowDimensions from "hooks/useWindowDimensions";
+import useTranslation from "~/hooks/useTranslation";
 
 function NavigationList() {
   const windowWidth = useWindowDimensions().width;
   const menuVisibilityBreakpoint = 780;
   const [isMenuVisible, setIsMenuVisible] = useState(windowWidth > menuVisibilityBreakpoint);
 
+  const [translation, setCurrentLanguage] = useTranslation();
+
+  function changeLanguage(language: string) {
+    setCurrentLanguage(language);
+  }
+
   function handleMenu() {
     setIsMenuVisible((isMenuVisible) => !isMenuVisible);
   }
+
   const duration = 300;
 
   const defaultStyle = {
@@ -46,11 +54,11 @@ function NavigationList() {
                   ...transitionStyles[state],
                 }}
               >
-                <NavigationItem isActive>Home</NavigationItem>
-                <NavigationItem>O nas</NavigationItem>
-                <NavigationItem>Oferta</NavigationItem>
-                <NavigationItem>Blog</NavigationItem>
-                <NavigationItem isHighlighted>Kontakt</NavigationItem>
+                <NavigationItem isActive>{translation.layout.header.navigation.home}</NavigationItem>
+                <NavigationItem>{translation.layout.header.navigation.about}</NavigationItem>
+                <NavigationItem>{translation.layout.header.navigation.offer}</NavigationItem>
+                <NavigationItem>{translation.layout.header.navigation.blog}</NavigationItem>
+                <NavigationItem isHighlighted>{translation.layout.header.navigation.contact}</NavigationItem>
               </ul>
               <ul
                 className={styles.navList}
@@ -59,14 +67,14 @@ function NavigationList() {
                   ...transitionStyles[state],
                 }}
               >
-                <NavigationItem>
+                <NavigationItem isActive={translation.name === "English"} onClick={() => changeLanguage("English")}>
                   <img
                     src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAt1BMVEWSmb66z+18msdig8La3u+tYX9IaLc7W7BagbmcUW+kqMr/q6n+//+hsNv/lIr/jIGMnNLJyOP9/fyQttT/wb3/////aWn+YWF5kNT0oqz0i4ueqtIZNJjhvt/8gn//WVr/6+rN1+o9RKZwgcMPJpX/VFT9UEn+RUX8Ozv2Ly+FGzdYZrfU1e/8LS/lQkG/mbVUX60AE231hHtcdMb0mp3qYFTFwNu3w9prcqSURGNDaaIUMX5FNW5wYt7AAAAAjklEQVR4AR3HNUJEMQCGwf+L8RR36ajR+1+CEuvRdd8kK9MNAiRQNgJmVDAt1yM6kSzYVJUsPNssAk5N7ZFKjVNFAY4co6TAOI+kyQm+LFUEBEKKzuWUNB7rSH/rSnvOulOGk+QlXTBqMIrfYX4tSe2nP3iRa/KNK7uTmWJ5a9+erZ3d+18od4ytiZdvZyuKWy8o3UpTVAAAAABJRU5ErkJggg=="
                     alt="Flag of United Kingdom"
                   />
                   English
                 </NavigationItem>
-                <NavigationItem isActive>
+                <NavigationItem isActive={translation.name === "Polish"} onClick={() => changeLanguage("Polish")}>
                   <img
                     src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAPFBMVEX/8fH67Ozz5ubt4OD+/v77+/v39/fo2Nj5AAD+jo79bW36X1/4T0/5QUHhAAD3MzP0Kir2ICDzERHtAACO5h0ZAAAAQUlEQVR4AQXBsRHDMAwAMTzNwvuvm5MVIECIZIEQZ4EQ7AAIGBABegsR+Z7VxChx7hCjhIwYJcTydcgQ1gv8Lhd/58kJTU6WtnIAAAAASUVORK5CYII"
                     alt="Flag of Poland"
