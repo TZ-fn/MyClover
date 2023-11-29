001f4f9e7e90620def4ec08c66c4f6a0d08cf2e9
Tomek <tomekz@vp.pl>
2023-11-29 14:37:46 +0100

    feat: add more data to the translations

--- src/assets/translations/englishTranslation.ts
+++ src/assets/translations/englishTranslation.ts
@@ -1,3 +1,32 @@
-const englishTranslation = {};
+const englishTranslation = {
+  name: "English",
+  layout: {
+    header: {
+      mainLogoLink: "Go to the main page.",
+      mainLogoAlt: "Logo My Clover",
+      navigation: {
+        home: "Home",
+        about: "About us",
+        offer: "Offer",
+        blog: "Blog",
+        contact: "Contact",
+      },
+    },
+    footer: {
+      contactInformation: "Contact information",
+      moreInformation: "More informations",
+      termsConditions: "Terms and Conditions",
+      privacyPolicy: "Privacy policy",
+      cookies: "Cookies",
+      contact: "Contact",
+      bottomBar: {
+        rights: "All rights reserved.",
+      },
+    },
+  },
+  pages: {
+    home: {},
+  },
+};
 
 export default englishTranslation;
--- src/assets/translations/polishTranslation.ts
+++ src/assets/translations/polishTranslation.ts
@@ -1,11 +1,32 @@
 const polishTranslation = {
-  pages: {
-    home: {
-      header: {
-        mainLogoLink: "Przejdź do strony głównej.",
-        mainLogoAlt: "Logo My Clover",
+  name: "Polish",
+  layout: {
+    header: {
+      mainLogoLink: "Przejdź do strony głównej.",
+      mainLogoAlt: "Logo My Clover",
+      navigation: {
+        home: "Home",
+        about: "O nas",
+        offer: "Oferta",
+        blog: "Blog",
+        contact: "Kontakt",
       },
     },
+    footer: {
+      contactInformation: "Informacje kontaktowe",
+      country: "Wielka Brytania",
+      moreInformation: "Więcej informacji",
+      termsConditions: "Regulamin",
+      privacyPolicy: "Polityka prywatności",
+      cookies: "Ciasteczka",
+      contact: "Kontakt",
+      bottomBar: {
+        rights: "Wszystkie prawa zastrzeżone.",
+      },
+    },
+  },
+  pages: {
+    home: {},
   },
 };
 
