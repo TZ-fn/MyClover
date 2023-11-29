910763d9209849994a7131204b7f665bfc36b054
Tomek <tomekz@vp.pl>
2023-11-29 14:39:42 +0100

    fix: add a missing onClick prop

--- src/components/Header/NavigationList/NavigationItem/NavigationItem.tsx
+++ src/components/Header/NavigationList/NavigationItem/NavigationItem.tsx
@@ -5,13 +5,16 @@ function NavigationItem({
   children,
   isActive,
   isHighlighted,
+  onClick,
 }: {
   children: ReactNode;
   isActive?: boolean;
   isHighlighted?: boolean;
+  onClick?: () => void;
 }) {
   return (
     <li
+      onClick={onClick}
       className={
         isActive ? styles.navListItemActive : isHighlighted ? styles.navListItemHighlighted : styles.navListItem
       }
