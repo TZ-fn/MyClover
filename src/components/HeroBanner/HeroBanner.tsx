bf65fa521c9df326eee9aadc1dc51ed63eefcc87
Tomek <tomekz@vp.pl>
2023-12-05 15:59:52 +0100

    feat: add the hero text rotation

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -1,17 +1,30 @@
 import useTranslation from "hooks/useTranslation";
 import styles from "./HeroBanner.module.scss";
+import { useEffect, useState } from "react";
 
 function HeroBanner() {
   const [translation] = useTranslation();
+  const [isFirstHeroActive, setIsFirstHeroActive] = useState(true);
+
+  useEffect(() => {
+    const heroInterval = setInterval(() => {
+      setIsFirstHeroActive((isFirstHeroActive) => !isFirstHeroActive);
+    }, 3000);
+    return () => clearInterval(heroInterval);
+  }, [isFirstHeroActive]);
 
   return (
     <div>
-      <div className={styles.heroOne}>
-        <h2>{translation.pages.home.heroOneText}</h2>
-      </div>
-      <div className={styles.heroTwo}>
-        <h2>{translation.pages.home.heroTwoText}</h2>
-      </div>
+      {isFirstHeroActive && (
+        <div className={styles.heroOne}>
+          <h2>{translation.pages.home.heroOneText}</h2>
+        </div>
+      )}
+      {!isFirstHeroActive && (
+        <div className={styles.heroTwo}>
+          <h2>{translation.pages.home.heroTwoText}</h2>
+        </div>
+      )}
     </div>
   );
 }
