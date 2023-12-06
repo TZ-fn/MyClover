b9f9c8279790608ab38d2765d81c47e05fa82a33
Tomek <tomekz@vp.pl>
2023-12-06 15:41:05 +0100

    feat: add hero image and more styling

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -1,3 +1,5 @@
+import heroImageOne from "assets/heroImageOne.png";
+import heroImageTwo from "assets/heroImageTwo.jpg";
 import useTranslation from "hooks/useTranslation";
 import styles from "./HeroBanner.module.scss";
 import { useEffect, useState } from "react";
@@ -6,23 +8,25 @@ function HeroBanner() {
   const [translation] = useTranslation();
   const [isFirstHeroActive, setIsFirstHeroActive] = useState(true);
 
-  useEffect(() => {
-    const heroInterval = setInterval(() => {
-      setIsFirstHeroActive((isFirstHeroActive) => !isFirstHeroActive);
-    }, 3000);
-    return () => clearInterval(heroInterval);
-  }, [isFirstHeroActive]);
+  // useEffect(() => {
+  //   const heroInterval = setInterval(() => {
+  //     setIsFirstHeroActive((isFirstHeroActive) => !isFirstHeroActive);
+  //   }, 5000);
+  //   return () => clearInterval(heroInterval);
+  // }, [isFirstHeroActive]);
 
   return (
-    <div>
+    <div className={styles.mainWrapper}>
       {isFirstHeroActive && (
         <div className={styles.heroOne}>
-          <h2>{translation.pages.home.heroOneText}</h2>
+          <h2 className={styles.heroTextOne}>{translation.pages.home.heroOneText}</h2>
+          <img src={heroImageOne} alt="" className={styles.heroImageOne} />
         </div>
       )}
       {!isFirstHeroActive && (
         <div className={styles.heroTwo}>
           <h2>{translation.pages.home.heroTwoText}</h2>
+          <img src="" alt="" className={styles.heroImageOne} />
         </div>
       )}
     </div>
