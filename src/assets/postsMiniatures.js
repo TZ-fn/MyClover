a48940e2c8303ec093e85be38ae7c4146d93a544
Tomek <tomekz@vp.pl>
2024-01-17 14:23:27 +0100

    feat: add more posts' miniatures

--- src/assets/postsMiniatures.js
+++ src/assets/postsMiniatures.js
@@ -1,7 +1,21 @@
 import miniature1 from "./posts/miniature_1.jpg";
 import miniature2 from "./posts/miniature_2.jpg";
 import miniature3 from "./posts/miniature_3.jpg";
+import miniature4 from "./posts/miniature_4.jpg";
+import miniature5 from "./posts/miniature_5.jpg";
+import miniature6 from "./posts/miniature_6.jpg";
+import miniature7 from "./posts/miniature_7.jpg";
+import miniature8 from "./posts/miniature_8.jpg";
 
-const postsMiniatures = [miniature1, miniature2, miniature3];
+const postsMiniatures = [
+  miniature1,
+  miniature2,
+  miniature3,
+  miniature4,
+  miniature5,
+  miniature6,
+  miniature7,
+  miniature8,
+];
 
 export default postsMiniatures;
