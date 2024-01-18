453a61c76899239130a57c54ff48516038a3e226
Tomek <tomekz@vp.pl>
2024-01-18 14:01:01 +0100

    feat: add missing posts' data

--- src/assets/translations/postsPolish.ts
+++ src/assets/translations/postsPolish.ts
@@ -3,6 +3,33 @@ const postsPolish = [
     descriptionHeader: "O czym warto pamiętać przed wyjazdem na wakacje?",
     description: "Sezon urlopowy w pełni, dlatego chcemy Wam przypomnieć o jednym.",
   },
+  {
+    descriptionHeader: "Trust – wszystko co musisz wiedzieć",
+    description:
+      "Trust to tak zwany dokument powierniczy. Potocznie, aby zrozumieć jego istotę mówimy, że to taki „testament” do polisy na życie.",
+  },
+  {
+    descriptionHeader: "Life Cover, czyli ubezpieczenie na życie",
+    description: "Dla kogo jest to odpowiednie rozwiązanie?",
+  },
+  {
+    descriptionHeader: "Event rodzinny z Clover",
+    description: "Eventy organizowane na świeżym powietrzu kochamy najbardziej.",
+  },
+  {
+    descriptionHeader: "Co warto wiedzieć o ochronie mienia?",
+    description: "Dlaczego nie zabezpieczyć swojego domu, samochodu, roweru, sprzętu i wielu cennych dla nas rzeczy?",
+  },
+  {
+    descriptionHeader: "Dlaczego warto ubezpieczyć firmę?",
+    description:
+      "Podnieść się po porażce można, ale jest to trudne, dlatego Clover proponuje rozwiązania mniej bolesne.",
+  },
+  {
+    descriptionHeader: "Ile kosztuje ubezpieczenie w UK?",
+    description:
+      "Dobre pytanie, cena zawsze jest zależna od wielu czynników dlatego, tylko po dokładnej analizie sytuacji konkretnej osoby możemy ustalić składkę.",
+  },
 ];
 
 export default postsPolish;
