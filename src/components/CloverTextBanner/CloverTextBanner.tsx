a59b56caa3f6d12346f3958985097c4dc4b7b1ca
Tomek <tomekz@vp.pl>
2024-01-30 14:59:49 +0100

    fix: change text to use the translations

--- src/components/CloverTextBanner/CloverTextBanner.tsx
+++ src/components/CloverTextBanner/CloverTextBanner.tsx
@@ -1,18 +1,18 @@
+import useTranslation from "~/hooks/useTranslation";
 import cloverSeparator from "../../assets/clover-separator.png";
 import styles from "./CloverTextBanner.module.scss";
 
 export default function CloverTextBanner() {
+  const [translation] = useTranslation();
+
   return (
     <div className={styles.textBannerContainer}>
       <div className={styles.textBannerInnerContainer}>
         <h3 className={styles.bannerHeader}>Clover - Family Cover</h3>
         <img src={cloverSeparator} className={styles.bannerSeparator} alt="" />
         <p className={styles.bannerText}>
-          Clover to coś więcej niż firma ubezpieczeniowa. Uwielbiamy kontakt z ludźmi, a nasza firma pragnie dostarczać
-          usługę ponadczasową. Oferujemy swój czas i doświadczenie, nie tylko w zakresie zawierania ubezpieczeń.
-          Pomożemy Ci wyszukać dogodną ofertę kredytową na dom, jak też zabezpieczymy Twój wymarzony świat. Gorąco
-          zachęcamy do zapoznania się z naszą szczegółową ofertą.
-          <span className={styles.bannerTextSpecial}>Zapraszamy również do współpracy.</span>
+          {translation.pages.home.aboutBanner}
+          <span className={styles.bannerTextSpecial}>{translation.pages.home.aboutBannerTextSpecial}</span>
         </p>
       </div>
     </div>
