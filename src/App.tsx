4fc036351a9a2ce49ed8094e0bec607ca0be5b3a
Tomek <tomekz@vp.pl>
2024-02-14 13:17:52 +0100

    fix: make the AboutSection more responsive on smaller viewports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Header from "components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import HeroBanner from "./components/HeroBanner/HeroBanner";
-import PostsGallery from "./components/PostsGallery/PostsGallery";
-import CloverTextBanner from "./components/CloverTextBanner/CloverTextBanner";
 import AboutSection from "./components/AboutSection/AboutSection";
+import CloverTextBanner from "./components/CloverTextBanner/CloverTextBanner";
+import PostsGallery from "./components/PostsGallery/PostsGallery";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
