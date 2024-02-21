79260f4368d283dd810080b42db840852d84611f
Tomek <tomekz@vp.pl>
2024-02-21 14:06:09 +0100

    feat: create a home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,19 @@
+import HeroBanner from "components/HeroBanner/HeroBanner";
+import AboutSection from "components/AboutSection/AboutSection";
+import CloverTextBanner from "components/CloverTextBanner/CloverTextBanner";
+import PostsGallery from "components/PostsGallery/PostsGallery";
+import ImageBanner from "components/BottomImage/ImageBanner";
+
+function Home() {
+  return (
+    <>
+      <HeroBanner />
+      <AboutSection />
+      <CloverTextBanner />
+      <PostsGallery />
+      <ImageBanner />
+    </>
+  );
+}
+
+export default Home;
