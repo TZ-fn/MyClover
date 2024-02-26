dcd1e489d655d0509f4ab3a7682bede7f35fa49b
Tomek <tomekz@vp.pl>
2024-02-26 17:24:18 +0100

    feat: add content to the 404 page

--- src/pages/NoMatch/NoMatch.tsx
+++ src/pages/NoMatch/NoMatch.tsx
@@ -1,5 +1,15 @@
+import useTranslation from "hooks/useTranslation";
+import styles from "./NoMatch.module.scss";
+
 function NoMatch() {
-  return <>404 No match</>;
+  const translation = useTranslation()[0].pages.noMatch;
+
+  return (
+    <div className={styles.noMatchContainer}>
+      <h2 className={styles.noMatchHeader}>{translation.noMatchHeader}</h2>
+      <p className={styles.noMatchText}>{translation.noMatchText}</p>
+    </div>
+  );
 }
 
 export default NoMatch;
