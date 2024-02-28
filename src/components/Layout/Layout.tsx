0d26099c4eec6cdb7f5093ab6aa748761d60c8f2
Tomek <tomekz@vp.pl>
2024-02-28 15:02:29 +0100

    feat: make all pages scroll to the top when changing the routes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { useEffect } from "react";
 
 function Layout() {
+  const { pathname } = useLocation();
+
+  // Automatically scrolls to top whenever pathname changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Header />
