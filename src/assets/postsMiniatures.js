10cf65b401d375176bce476041dfdf78882cf211
Tomek <tomekz@vp.pl>
2024-03-12 16:38:44 +0100

    feat: change posts miniatures to webp files

--- src/assets/postsMiniatures.js
+++ src/assets/postsMiniatures.js
@@ -1,10 +1,10 @@
-import miniature1 from "./posts/miniature_1.jpg";
-import miniature2 from "./posts/miniature_2.jpg";
-import miniature3 from "./posts/miniature_3.jpg";
-import miniature4 from "./posts/miniature_4.jpg";
-import miniature5 from "./posts/miniature_5.jpg";
-import miniature6 from "./posts/miniature_6.jpg";
-import miniature7 from "./posts/miniature_7.jpg";
+import miniature1 from "./posts/miniature_1.webp";
+import miniature2 from "./posts/miniature_2.webp";
+import miniature3 from "./posts/miniature_3.webp";
+import miniature4 from "./posts/miniature_4.webp";
+import miniature5 from "./posts/miniature_5.webp";
+import miniature6 from "./posts/miniature_6.webp";
+import miniature7 from "./posts/miniature_7.webp";
 
 const postsMiniatures = [miniature1, miniature2, miniature3, miniature4, miniature5, miniature6, miniature7];
 
