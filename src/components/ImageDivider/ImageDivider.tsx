bd2647d65e941657049546563735cb46c18d936a
Tomek <tomekz@vp.pl>
2024-03-18 21:47:32 +0100

    fix: refactor the component styling

--- src/components/ImageDivider/ImageDivider.tsx
+++ src/components/ImageDivider/ImageDivider.tsx
@@ -1,5 +1,9 @@
 import styles from "./ImageDivider.module.scss";
 
 export default function ImageDivider({ isUpwards }: { isUpwards: boolean }) {
-  return <div className={`${styles.imageDivider} ${isUpwards ? styles.upwards : styles.downwards}`}></div>;
+  return (
+    <div className={styles.imageDividerContainer}>
+      <div className={`${styles.imageDivider} ${isUpwards ? styles.upwards : styles.downwards}`}></div>
+    </div>
+  );
 }
