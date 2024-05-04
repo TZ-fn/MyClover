4d74e2bd5c6e23b4e060ef06ed19c0807ae6fcc6
Tomek <tomekz@vp.pl>
2024-05-04 13:39:52 +0200

    fix: rename the files, add the offers

---
+++ src/components/AboutSection/OffersList/OffersList.tsx
@@ -0,0 +1,23 @@
+import useTranslation from "hooks/useTranslation";
+import styles from "./OffersList.module.scss";
+import Offer from "./Offer/Offer";
+
+export default function OffersList() {
+  const [translation] = useTranslation();
+
+  const offers = translation.pages.aboutUs.sectionTwo;
+
+  return (
+    <div className={styles.servicesContainer}>
+      <h3>{translation.pages.aboutUs.sectionTwo.header}</h3>
+      <ul className={styles.servicesList}>
+        <Offer title={offers.offer1} />
+        <Offer title={offers.offer2} />
+        <Offer title={offers.offer3} />
+        <Offer title={offers.offer4} />
+        <Offer title={offers.offer5} />
+        <Offer title={offers.offer6} />
+      </ul>
+    </div>
+  );
+}
