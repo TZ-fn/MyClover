2a4aeaec677b7e713be81e3212efb3e1182d0c30
Tomek <tomekz@vp.pl>
2024-05-12 21:36:37 +0200

    feat: add plus/minus icons to the questions

--- src/components/FAQ/Question/Question.tsx
+++ src/components/FAQ/Question/Question.tsx
@@ -1,9 +1,21 @@
+import iconPlus from "assets/icons/iconPlus.svg";
+import iconMinus from "assets/icons/iconMinus.svg";
 import styles from "./Question.module.scss";
 
-export default function Question({ FAQitem }: { FAQitem: { question: string; answer: string } }) {
+export default function Question({
+  FAQitem,
+  isOpen,
+}: {
+  FAQitem: { question: string; answer: string };
+  isOpen: boolean;
+}) {
+  console.log(isOpen);
   return (
     <div className={styles.questionContainer}>
-      <p className={styles.question}>{FAQitem.question}</p>
+      <div className={styles.questionInnerContainer}>
+        <img className={styles.questionIcon} src={isOpen ? iconMinus : iconPlus} alt="" />
+        <p className={styles.question}>{FAQitem.question}</p>
+      </div>
       <p className={styles.answer}>{FAQitem.answer}</p>
     </div>
   );
