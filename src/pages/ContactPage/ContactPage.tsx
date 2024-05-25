abebd0542d35a5543ef68b994b1f7337a1312a87
Tomek <tomekz@vp.pl>
2024-05-25 16:58:37 +0200

    feat: add header for the ContactPage

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,5 +1,16 @@
+import ImageDivider from "components/ImageDivider/ImageDivider";
+import ImageHeader from "components/ImageHeader/ImageHeader";
+import useTranslation from "hooks/useTranslation";
+
 function ContactPage() {
-  return <>Contact Page</>;
+  const [translation] = useTranslation();
+
+  return (
+    <>
+      <ImageHeader pageName={translation.header.navigation.contact} backgroundImage="src/assets/contact.webp" />
+      <ImageDivider isUpwards={false} />
+    </>
+  );
 }
 
 export default ContactPage;
