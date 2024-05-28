ce07928d7ea48c280c37bd9f3ea655285bfb7533
Tomek <tomekz@vp.pl>
2024-05-28 17:00:21 +0200

    feat: add more content and styling to the contact form

--- src/components/ContactPageMain/ContactForm/ContactForm.tsx
+++ src/components/ContactPageMain/ContactForm/ContactForm.tsx
@@ -7,11 +7,24 @@ export default function ContactForm() {
 
   return (
     <form className={styles.mainContainer}>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+      <h3>{contactPageTranslation.contactForm.header}</h3>
+      <p>{contactPageTranslation.contactForm.text1}</p>
+      <input type="text" placeholder={contactPageTranslation.contactForm.form1Placeholder} />
+      <input type="text" placeholder={contactPageTranslation.contactForm.form2Placeholder} />
+      <select>
+        <option value={contactPageTranslation.contactForm.form3Options.insurance}>
+          {contactPageTranslation.contactForm.form3Options.insurance}
+        </option>
+        <option value={contactPageTranslation.contactForm.form3Options.mortgage}>
+          {contactPageTranslation.contactForm.form3Options.mortgage}
+        </option>
+      </select>
+      <input type="text" placeholder={contactPageTranslation.contactForm.form4Placeholder} />
+      <input type="checkbox" />
+      <p>{contactPageTranslation.contactForm.formDisclaimer1}</p>
+      <span>{contactPageTranslation.contactForm.formDisclaimer2}</span>
+      <p>{contactPageTranslation.contactForm.formDisclaimer3}</p>
+      <p>{contactPageTranslation.contactForm.formDisclaimer4}</p>
       <button>{contactPageTranslation.contactForm.send}</button>
     </form>
   );
