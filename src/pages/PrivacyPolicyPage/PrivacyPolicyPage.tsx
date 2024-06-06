dcd7e6ee1f33e73dd0d4ae84dc028eaac131620c
Tomek <tomekz@vp.pl>
2024-06-06 14:03:41 +0200

    feat: add a header to the privacy policy page

--- src/pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
@@ -1,8 +1,15 @@
+import ImageDivider from "components/ImageDivider/ImageDivider";
+import ImageHeader from "components/ImageHeader/ImageHeader";
 import PrivacyPolicy from "components/PrivacyPolicy/PrivacyPolicy";
+import useTranslation from "hooks/useTranslation";
 
 function PrivacyPolicyPage() {
+  const [translation] = useTranslation();
+
   return (
     <>
+      <ImageHeader pageName={translation.pages.privacyPolicy.mainHeader} backgroundImage="src/assets/clover.webp" />
+      <ImageDivider isUpwards={false} />
       <PrivacyPolicy />
     </>
   );
