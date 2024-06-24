8625969ea57def1f05bfce3b267b277b8157874b
Tomek <tomekz@vp.pl>
2024-06-24 16:00:39 +0200

    feat: add content to the careers page

--- src/pages/CareersPage/CareersPage.tsx
+++ src/pages/CareersPage/CareersPage.tsx
@@ -1,3 +1,4 @@
+import CareersPageMain from "components/CareersPageMain/CareersPageMain";
 import ImageDivider from "components/ImageDivider/ImageDivider";
 import ImageHeader from "components/ImageHeader/ImageHeader";
 import useTranslation from "hooks/useTranslation";
@@ -7,8 +8,9 @@ function CareersPage() {
 
   return (
     <>
-      <ImageHeader pageName={translation.pages.careers.mainHeader} backgroundImage="src/assets/careers.webp" />
+      <ImageHeader pageName={translation.pages.careers.title} backgroundImage="src/assets/careers.webp" />
       <ImageDivider isUpwards={false} />
+      <CareersPageMain />
     </>
   );
 }
