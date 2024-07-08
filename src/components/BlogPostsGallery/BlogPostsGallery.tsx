e04c79c2f7cf1adf4dedd8220ef0bb83350ce2e7
Tomek <tomekz@vp.pl>
2024-07-08 16:53:24 +0200

    fix: add the missing "key" prop

--- src/components/BlogPostsGallery/BlogPostsGallery.tsx
+++ src/components/BlogPostsGallery/BlogPostsGallery.tsx
@@ -11,6 +11,7 @@ export default function BlogPostsGallery() {
       {translation.pages.posts.map((post, index) => {
         return (
           <PostsGalleryItem
+            key={index}
             miniature={mainPostsData[index]?.miniature}
             descriptionHeader={post.descriptionHeader}
             description={post.description}
