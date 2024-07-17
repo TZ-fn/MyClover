95ad180c79cc62598c221563910694a7358108f1
Tomek <tomekz@vp.pl>
2024-07-17 18:26:03 +0200

    feat: add content to the Holiday page

--- src/components/Posts/HolidayContent/HolidayContent.tsx
+++ src/components/Posts/HolidayContent/HolidayContent.tsx
@@ -1,8 +1,28 @@
 import useTranslation from "hooks/useTranslation";
+import TextItem from "components/shared/TextItem/TextItem";
+import TextList from "components/shared/TextList/TextList";
 import styles from "./HolidayContent.module.scss";
 
 export default function HolidayContent() {
   const [translation] = useTranslation();
 
-  return <main className={styles.mainContainer}>Holiday</main>;
+  return (
+    <main className={styles.mainContainer}>
+      <h3 className={styles.mainHeader}>{translation.pages.postsData.holiday.mainHeader}</h3>
+      {translation.pages.postsData.holiday.items.map((item) => {
+        if (item.header || item.text) {
+          return <TextItem key={item.header} header={item.header} text={item.text} />;
+        }
+        if (item.listDescription) {
+          return (
+            <TextList
+              key={item.listDescription}
+              listDescription={item.listDescription}
+              listElements={item.listElements}
+            />
+          );
+        }
+      })}
+    </main>
+  );
 }
