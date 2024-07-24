ee59b4cf7d4c74b8e68f4faad04711fb00a790dd
Tomek <tomekz@vp.pl>
2024-07-24 15:32:41 +0200

    feat: add a background image for the Trust page

--- src/pages/Posts/Trust/TrustPage.tsx
+++ src/pages/Posts/Trust/TrustPage.tsx
@@ -3,7 +3,7 @@ import ImageHeader from "components/ImageHeader/ImageHeader";
 import useTranslation from "hooks/useTranslation";
 import PageTitle from "components/PageTitle/PageTitle";
 import TrustContent from "components/TrustContent/TrustContent";
-import holiday from "assets/holiday.webp";
+import trust from "assets/postsBackgrounds/trust.webp";
 
 function TrustPage() {
   const [translation] = useTranslation();
@@ -11,7 +11,7 @@ function TrustPage() {
   return (
     <>
       <PageTitle title={translation.pages.postsData.trust.mainHeader} />
-      <ImageHeader pageName={translation.pages.postsData.trust.mainHeader} backgroundImage={holiday} />
+      <ImageHeader pageName={translation.pages.postsData.trust.mainHeader} backgroundImage={trust} />
       <ImageDivider isUpwards={false} />
       <TrustContent />
     </>
